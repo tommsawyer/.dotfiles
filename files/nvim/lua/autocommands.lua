5aeceb6f812064b677c9e4f6eb92e8950605c867
Denis Maximov <mortraineymusic@gmail.com>
2023-03-25 18:44:30 +0300

    highlight on yank

---
+++ files/nvim/lua/autocommands.lua
@@ -0,0 +1,7 @@
+local api = vim.api
+
+local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+  command = "silent! lua vim.highlight.on_yank()",
+  group = yankGrp,
+})
