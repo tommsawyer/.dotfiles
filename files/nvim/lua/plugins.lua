49efb157a7914e24c611201c8070af0f9786edba
Denis Maximov <mortraineymusic@gmail.com>
2023-03-26 21:26:27 +0300

    refactor plugins section

--- files/nvim/lua/plugins.lua
+++ files/nvim/lua/plugins.lua
@@ -6,8 +6,8 @@ return require("packer").startup(function(use)
   -- managing files
   use {
     "nvim-telescope/telescope.nvim", tag = "0.1.1",
-    requires = { {"nvim-lua/plenary.nvim"} }
-  } -- fuzzy-finder
+    requires = { { "nvim-lua/plenary.nvim" } }
+  }                             -- fuzzy-finder
   use "nvim-tree/nvim-tree.lua" -- project tree
 
   -- LSP support
@@ -35,44 +35,29 @@ return require("packer").startup(function(use)
 
   -- autoformatting
   use "jose-elias-alvarez/null-ls.nvim"
-  
+
   -- appearance
   use {
-    "lunarvim/darkplus.nvim" ,              -- main theme
-    "lewis6991/gitsigns.nvim",              -- git signs
-    "lukas-reineke/indent-blankline.nvim" , -- indentation level lines
-    "stevearc/dressing.nvim",               -- better ui
-    "onsails/lspkind.nvim",                 -- completion icons
-  } 
-  use {
-    'kosayoda/nvim-lightbulb',
-    requires = 'antoinemadec/FixCursorHold.nvim',
-  }
-  use {
-    "folke/trouble.nvim",
-    requires = "nvim-tree/nvim-web-devicons",
+    "lunarvim/darkplus.nvim",              -- main theme
+    "lewis6991/gitsigns.nvim",             -- git signs
+    "lukas-reineke/indent-blankline.nvim", -- indentation level lines
+    "stevearc/dressing.nvim",              -- better ui
+    "onsails/lspkind.nvim",                -- completion icons
+    "nvim-tree/nvim-web-devicons",         -- better icons
+    "antoinemadec/FixCursorHold.nvim",
+    "kosayoda/nvim-lightbulb",             -- hints for lsp actions
+    "folke/trouble.nvim",                  -- better ui for linters
   }
 
   -- improving nvim
-  use("nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"}) -- AST
+  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" }) -- AST
   use {
-    "machakann/vim-sandwich", -- surround text with brackets/tags
-    "tpope/vim-unimpaired",   -- additional paired shortcuts
-    "windwp/nvim-autopairs",  -- auto-add closing quotes/brackets
-    "numToStr/Comment.nvim",  -- commenting
-    "b0o/schemastore.nvim",   -- schemas for yaml/json
+    "machakann/vim-sandwich",                                   -- surround text with brackets/tags
+    "tpope/vim-unimpaired",                                     -- additional paired shortcuts
+    "windwp/nvim-autopairs",                                    -- auto-add closing quotes/brackets
+    "numToStr/Comment.nvim",                                    -- commenting
+    "b0o/schemastore.nvim",                                     -- schemas for yaml/json
+    "nvim-treesitter/nvim-treesitter-textobjects",              -- additional text objects
+    "Wansmer/treesj",                                           -- spilt/join lines
   }
-  -- additional text objects from treesitter
-  use({
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    after = "nvim-treesitter",
-    requires = "nvim-treesitter/nvim-treesitter",
-  })
-  -- split/join lines
-  use({
-    "Wansmer/treesj",
-    requires = { "nvim-treesitter" },
-  })
-  use "takac/vim-hardtime" -- break bad habits
 end)
-
