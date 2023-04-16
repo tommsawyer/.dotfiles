6bbe8f7c48a5e9e19113f72324cdccc0bc9dfda1
Denis Maximov <mortraineymusic@gmail.com>
2023-04-16 14:43:37 +0300

    add nvim-tree settings

--- files/nvim/after/plugin/nvim-tree.lua
+++ files/nvim/after/plugin/nvim-tree.lua
@@ -1,4 +1,16 @@
-require("nvim-tree").setup()
+require("nvim-tree").setup {
+  view = {
+    hide_root_folder = true,
+  },
+  renderer = {
+    icons = {
+      show = {
+        git = false,
+        modified = false,
+      },
+    },
+  },
+}
 
 vim.keymap.set('n', '<leader><leader>', '<Cmd>NvimTreeToggle<CR>')
 vim.keymap.set('n', '<leader>a', ':NvimTreeFindFile<CR>')
