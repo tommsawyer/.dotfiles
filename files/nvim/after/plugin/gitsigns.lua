a97f16b566df2cbf66f9f330d88a4025c15f9e9a
Denis Maximov <mortraineymusic@gmail.com>
2023-04-22 16:37:33 +0300

    add keymap for git blame of current line

--- files/nvim/after/plugin/gitsigns.lua
+++ files/nvim/after/plugin/gitsigns.lua
@@ -1,5 +1,6 @@
 require('gitsigns').setup({
   on_attach = function()
     vim.keymap.set("n", "<leader>gu", ":Gitsigns reset_hunk<CR>")
+    vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>")
   end
 })
