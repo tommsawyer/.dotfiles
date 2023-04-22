b410cae72847f238a5548617220a9600dc95023c
Denis Maximov <mortraineymusic@gmail.com>
2023-04-22 20:40:04 +0300

    simplify move window mapping

--- files/nvim/lua/mappings.lua
+++ files/nvim/lua/mappings.lua
@@ -1,21 +1,23 @@
 vim.g.mapleader = ","
 
 local function moveWindow(direction)
-  local currentWindow = vim.fn.winnr()
-
-  vim.api.nvim_command("wincmd " .. direction)
-  if vim.fn.winnr() == currentWindow then
-    if direction == "j" or direction == "k" then
-      vim.api.nvim_command("wincmd s")
-    else
-      vim.api.nvim_command("wincmd v")
-    end
+  return function()
+    local currentWindow = vim.fn.winnr()
 
     vim.api.nvim_command("wincmd " .. direction)
+    if vim.fn.winnr() == currentWindow then
+      if direction == "j" or direction == "k" then
+        vim.api.nvim_command("wincmd s")
+      else
+        vim.api.nvim_command("wincmd v")
+      end
+
+      vim.api.nvim_command("wincmd " .. direction)
+    end
   end
 end
 
-vim.keymap.set('n', '<C-h>', function() moveWindow("h") end)
-vim.keymap.set('n', '<C-j>', function() moveWindow("j") end)
-vim.keymap.set('n', '<C-k>', function() moveWindow("k") end)
-vim.keymap.set('n', '<C-l>', function() moveWindow("l") end)
+vim.keymap.set('n', '<C-h>', moveWindow("h"))
+vim.keymap.set('n', '<C-j>', moveWindow("j"))
+vim.keymap.set('n', '<C-k>', moveWindow("k"))
+vim.keymap.set('n', '<C-l>', moveWindow("l"))
