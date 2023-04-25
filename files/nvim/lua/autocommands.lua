11a92d712422cd3e427da89f011066770a44d0c3
Denis Maximov <mortraineymusic@gmail.com>
2023-04-25 18:39:54 +0300

    use visual highlight for yank

--- files/nvim/lua/autocommands.lua
+++ files/nvim/lua/autocommands.lua
@@ -2,6 +2,6 @@ local api = vim.api
 
 local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
 api.nvim_create_autocmd("TextYankPost", {
-  command = "silent! lua vim.highlight.on_yank()",
+  command = "silent! lua vim.highlight.on_yank({ higroup = \"Visual\" })",
   group = yankGrp,
 })
