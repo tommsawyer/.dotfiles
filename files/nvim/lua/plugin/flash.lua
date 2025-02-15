1f3809270514b6210270ec7e39e3fa0223073a62
Denis Maximov <mortraineymusic@gmail.com>
2025-02-15 21:45:35 +0300

    enable flash.nvim on search

--- files/nvim/lua/plugin/flash.lua
+++ files/nvim/lua/plugin/flash.lua
@@ -1,7 +1,13 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    modes = {
+      search = {
+        enabled = true,
+      },
+    },
+  },
   keys = {
     { "r", mode = "o",          function() require("flash").remote() end,     desc = "Remote Flash" },
     { "S", mode = { "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
--- files/nvim/lua/plugin/snacks.lua
+++ files/nvim/lua/plugin/snacks.lua
@@ -4,13 +4,6 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    -- bigfile = { enabled = true },
-    -- dashboard = { enabled = true },
-    -- explorer = { enabled = true },
-    -- input = { enabled = true },
     picker = {
       enabled = true,
       layout = {
